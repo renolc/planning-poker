3df20abacf1d594f58fe92d4525fd62ba1237143
Phillip Gibson <gibson.phillip.j@gmail.com>
2017-05-23 16:37:46 -0400

    ESC to cancel. Ctrl+Enter to save Closes #3

--- views/room/task/edit.js
+++ views/room/task/edit.js
@@ -1,6 +1,22 @@
 module.exports = (room) => `
   <form id="task-edit-form" ic-post-to="/${room.name}/task" ic-replace-target="true">
     <textarea style="height: 15rem" name="task" autofocus>${room.task || ''}</textarea>
-    <input type="submit" value="save"> <a class="button button-clear" ic-get-from="/${room.name}/task/full" ic-target="#task-edit-form" ic-replace-target="true">cancel</a>
+    <input id="save" type="submit" value="save"> <a id="cancel" class="button button-clear" ic-get-from="/${room.name}/task/full" ic-target="#task-edit-form" ic-replace-target="true">cancel</a>
   </form>
+  <script>
+    const textarea = document.querySelector('textarea')
+    const cancel = document.querySelector('#cancel')
+    const save = document.querySelector('#save')
+    var ctrlDown = false
+
+    textarea.onkeydown = (e) => {
+      if (e.key === 'Control') return ctrlDown = true
+    }
+
+    textarea.onkeyup = (e) => {
+      if (e.key === 'Escape') return cancel.click()
+      if (e.key === 'Control') return ctrlDown = false
+      if (e.key === 'Enter' && ctrlDown) return save.click()
+    }
+  </script>
 `
